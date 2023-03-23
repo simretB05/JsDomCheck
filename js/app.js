b91037da55d584ebb87225e1fdc81a365d3abc25
simretB05 <happysimret@gmail.com>
2023-03-23 17:40:12 -0600

    accessing elements with DOM

---
+++ js/app.js
@@ -0,0 +1,4 @@
+let get_books_p_tag = document.querySelector(`p`)
+let get_all_p_tages = document.querySelectorAll( `.body` )
+let get_articles_by_id = document.getElementById( `buy_books` )
+let get_articles_by_class =document.getElementsByClassName(`.body`)
